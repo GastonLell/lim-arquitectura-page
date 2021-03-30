8285ed1ffb7e667cdf1451efb42f784c6802b5ca
YairAresche <areschelainoyair@gmail.com>
2021-03-30 12:08:04 -0300

    cambio en contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -2,6 +2,7 @@ const Contacto = () => {
     return(
         <section>
             <h2>Seccion contactos</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eligendi?</p>
         </section>
     )
 }
