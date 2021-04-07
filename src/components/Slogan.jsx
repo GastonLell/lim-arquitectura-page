8844fb5ec0331fba23def00a0b8a5b3b4c4aa054
GastonLell <gastonglell@gmail.com>
2021-04-07 12:35:54 -0300

    carousel en inicio

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -1,39 +1,6 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-import ImgInicio1 from "../assets/images/Renders-inico/Inicio1.jpg";
-import ImgInicio2 from "../assets/images/Renders-inico/Inicio2.jpg";
-import ImgInicio3 from "../assets/images/Renders-inico/Inicio3.jpg";
-
-import Carousel from "./Carousel";
-
-const items = [
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio1,
-  },
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio2,
-  },
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio3,
-  },
-];
 const Slogan = () => {
   return (
-    <Container fluid className="p-0">
-      <Row className="m-0">
-        <Col className="p-0">
-          <Carousel items={items} tamaño="w-100"/>
-        </Col>
-      </Row>
-    </Container>
+    <h2>slogan</h2>
   );
 };
 export default Slogan;
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,12 +1,44 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import ImgInicio1 from "../assets/images/Renders-inico/Inicio1.jpg";
+import ImgInicio2 from "../assets/images/Renders-inico/Inicio2.jpg";
+import ImgInicio3 from "../assets/images/Renders-inico/Inicio3.jpg";
+
+
 // componentes
 import Menu from "../components/Menu";
-import Slogan from "../components/Slogan";
+import Carousel from "../components/Carousel";
 
+const items = [
+  {
+    nombre: "LIM arquitectura",
+    categoria: "inicio",
+    srcImagen: ImgInicio1,
+  },
+  {
+    nombre: "LIM arquitectura",
+    categoria: "inicio",
+    srcImagen: ImgInicio2,
+  },
+  {
+    nombre: "LIM arquitectura",
+    categoria: "inicio",
+    srcImagen: ImgInicio3,
+  },
+];
 const Inicio = () => {
   return (
     <div id="inicio">
       <Menu />
-      <Slogan />
+      <Container fluid className="p-0">
+      <Row className="m-0">
+        <Col className="p-0">
+          <Carousel items={items} tamaño="w-100"/>
+        </Col>
+      </Row>
+    </Container>
     </div>
   );
 };
