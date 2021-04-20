8fd7d243cc71b7336193d30461e6807e4cc7eca0
GastonLell <gastonglell@gmail.com>
2021-04-20 16:42:18 -0300

    animacion y carousel inicio

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // dependencias
 
+// sass y componentes react bootstrap
 import './scss/app.css';
-// componentes react bootstrap
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import {Container, Row, Col} from "react-bootstrap";
+
 // vistas secciones
 import Inicio from "./pages/Inicio";
+import Menu from "./components/Menu";
 import Nosotros from "./pages/Nosotros";
 import Servicios from "./pages/Servicios";
 import Proyectos from "./pages/Proyectos";
 import Contacto from "./pages/Contacto";
-import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 
 import ProyectosContext from "./store/ProyectosContext";
