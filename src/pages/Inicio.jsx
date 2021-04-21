fc6047ccf542721a218b3856a73775c123c19c38
GastonLell <gastonglell@gmail.com>
2021-04-21 14:14:35 -0300

    imagenes proyectos

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,32 +1,11 @@
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 
-import ImgInicio1 from "../assets/images/Renders-inico/Inicio1.jpg";
-import ImgInicio2 from "../assets/images/Renders-inico/Inicio2.jpg";
-import ImgInicio3 from "../assets/images/Renders-inico/Inicio3.jpg";
 import ImgLogo from "../assets/images/limArquitectura-negro.svg";
 import ImgSimbolo from "../assets/images/Simbolo-alto.svg";
 
 // componentes
-// import Carousel from "../components/Carousel";
 import Slogan from "../components/Slogan";
 
-const items = [
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio1,
-  },
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio2,
-  },
-  {
-    nombre: "LIM arquitectura",
-    categoria: "inicio",
-    srcImagen: ImgInicio3,
-  },
-];
 const Inicio = () => {
   return (
     <div id="inicio">
