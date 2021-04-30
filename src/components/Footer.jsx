5389e076605ba1cd8178114830fad6275c22e92c
YairAresche <areschelainoyair@gmail.com>
2021-04-30 19:09:25 -0300

    logos y simbolo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ import Col from 'react-bootstrap/Col';
 import simboloBlanco from "../assets/images/simbolo-blanco.svg";
 import behance from "../assets/images/vector behance.jpg";
 import instagram from "../assets/images/vector instagram.jpg";
+import facebook from "../assets/images/facebook.png";
+import linkedin from "../assets/images/linkedin.jpg";
 
 const Footer = () => {
     return (
@@ -19,8 +21,10 @@ const Footer = () => {
                         <p>También encontranos en</p>
                         
                         <div className="footer__contenedorVectores">
-                        <img src={instagram} className="footer__vector p-1 mr-2" alt="Instagram"/>
-                        <img src={behance} className="footer__vector p-1" alt="Behance"/>
+                        <img src={instagram} className="footer__vector mr-2" alt="Instagram"/>
+                        <img src={facebook} className="footer__vector mr-2" alt="Facebook"/>
+                        <img src={linkedin} className="footer__vector mr-2" alt="Linkedin"/>
+                        <img src={behance} className="footer__vector" alt="Behance"/>
                         </div>
                 </Col>
             </Row>
