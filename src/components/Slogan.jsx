5005f260c7d4667f03be2795bdc7dee53ab64b73
GastonLell <gastonglell@gmail.com>
2021-05-03 18:52:42 -0300

    slogan centrado

--- src/components/Slogan.jsx
+++ src/components/Slogan.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container'
 const Slogan = () => {
   return (
 
-    <Container fluid className="slogan__contenedor my-2">
+    <Container fluid className="slogan__contenedor ">
       <Container>
         <p className="sloganLato ">Pensamos los proyectos para hoy pero también para el mañana, adaptandose a un medio cambiante que respeta los recursos naturales y generan soluciones sostenibles.</p>
       </Container>
